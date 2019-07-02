f04a2881c1ad3dba592b56095b3573d1a66cb49d
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-07-02 09:19:08 +0200

    solution: Got a working solution!

--- src/index.ts
+++ src/index.ts
@@ -22,13 +22,17 @@ createConnection().then(async connection => {
         user.spots = [spot];
         await connection.manager.save(user);
         await connection.manager.save(spot);
+        await connection.manager.save(new Spot());
+        await connection.manager.save(new Spot());
         console.log('setup done');
     }
+
+    // My way to do it
     const spotRepo = getRepository(Spot);
     const spot = await spotRepo.createQueryBuilder('spot')
         .innerJoin('spot.user', 'user')
         .where('user.id = :id', { id: 1 })
-        .andWhere('spot.id = :id', {id: 1})
+        .andWhere('spot.id = :id', { id: 1 })
         .getOne();
     if (spot !== undefined) {
         console.log(spot);
@@ -41,4 +45,18 @@ createConnection().then(async connection => {
     } else {
         console.log(`No spot? with user id ${1}`);
     }
+
+    // Solition by https://stackoverflow.com/users/2707363/zenbeni
+    // https://stackoverflow.com/a/56836624/1770432
+    const thingRepo = getRepository(Thing);
+    const things = await thingRepo.createQueryBuilder('thing')
+        .leftJoinAndSelect('thing.spot', 'spotThing')
+        .leftJoinAndSelect('spotThing.user', 'userSpot')
+        .where('spotThing.id = :spotId', { spotId: 2 })
+        .andWhere('userSpot.id = :userId', { userId: 1 })
+        .getMany();
+    console.log(things);
+
+    await connection.close();
+
 }).catch(error => console.log(error));
